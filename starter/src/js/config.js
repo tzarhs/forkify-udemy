49bef7a133e7f1908d48450e907f934b1a9bc2ff
tzarhs <j.tzaris@gmail.com>
2023-11-13 10:49:42 +0200

    Initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '28db90a2-bb63-4776-a56b-928b08d4eca2';
+export const MODAL_CLOSE_SEC = 2.5;
